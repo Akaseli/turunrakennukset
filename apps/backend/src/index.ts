943d9a5db27d3c1a7bac70c21a7ad3cb96f61491
Akseli Lehtniemi <60463103+Akaseli@users.noreply.github.com>
2025-06-02 13:12:00 +0300

    Fixed data fetched not being updated.

--- apps/backend/src/index.ts
+++ apps/backend/src/index.ts
@@ -10,7 +10,7 @@ const app = express();
 const { Pool } = pg
 
 //7 päivää
-const updateData = setInterval(updateSources, 7 * 24 * 60 * 60 * 1000);
+const updateData = setInterval(StartUpdate, 7 * 24 * 60 * 60 * 1000);
 
 function StartUpdate(){
     updateSources();
